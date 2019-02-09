3984f6c748c69870d8c421ce8a8e71fb955a6589
onogues <olnogues@gmail.com>
2019-02-09 23:10:48 +0100

    Themes

---
+++ CSS/configs/ThemeBespoke.py
@@ -0,0 +1,9 @@
+
+class BespokeTheme(object):
+  name='bespoke'
+  colors = ['yellow', '#cfd8dc', '#b0bec5', '#90a4ae', '#78909c', '#607d8b', '#546e7a', '#455a64', '#37474f', '#263238']
+  greys = ['#FFFFFF', '#f5f5f5', '#eeeeee', '#e0e0e0', '#bdbdbd', '#9e9e9e', '#757575', '#616161', '#424242', '#212121', '#000000']
+  charts = ['#e3f2fd', '#bbdefb', '#90caf9', '#e1f5fe', '#b3e5fc', '#81d4fa', '#e0f2f1', '#b2dfdb', '#80cbc4', '#e8f5e9', '#c8e6c9', '#a5d6a7', '#ffebee', '#ffcdd2', '#ef9a9a', '#f3e5f5', '#e1bee7', '#ce93d8', '#ede7f6', '#d1c4e9', '#b39ddb', '#e8eaf6', '#c5cae9', '#9fa8da', '#fffde7', '#fff9c4', '#fff59d', '#fff3e0', '#ffe0b2', '#ffcc80', '#efebe9', '#d7ccc8', '#bcaaa4']
+  warning = ('#FFF3CD', '#e2ac00')
+  danger = ('#F8D7DA', '#C00000')
+  success = ('#F8D7DA', '#C00000')
